5c5a89d83bb127f6d8cd4b5324fe6b846ff5c465
ZhangShu <zhangshuyx@gmail.com>
2021-05-28 00:48:36 +0800

    create

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+fn main() {
+    fn plus_one(x: Option<i32>) -> Option<i32> {
+        match x {
+            None => None,
+            Some(i) => Some(i + 1),
+        }
+    }
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
+    //println!("Hello, world!");
+
+    let some_u8_value = 0u16;
+    match some_u8_value {
+        1 => println!("one"),
+        3 => println!("three"),
+        5 => println!("five"),
+        7 => println!("seven"),
+        _ => (),
+    }
+}
